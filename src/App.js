9c4a2d9d08afb97a75e260e50b6cd723857ce64c
gur-p-reet <gsl.gurpreet@gmail.com>
2021-01-26 00:06:49 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render(){
+     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <ul className="mainNav">
+        <li><a href="" alt="" >Home </a></li>
+        <li><a href="" alt="" >Furniture</a></li>
+        <li><a href="" alt="" >Kitchen Cabinets</a></li>
+        <li><a href="" alt="" >Doors/Windows</a></li>
+        <li><a href="" alt="" >Crafts</a></li>
+        <li><a href="" alt="" >Wood Blocks</a></li>
+        <li className="push"><a href="" alt="" >login/signup</a></li>
+      </ul>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
+  }
 }
 
 export default App;
